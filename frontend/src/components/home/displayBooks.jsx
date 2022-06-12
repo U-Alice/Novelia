5fdb9c084df1444011ca299e6aeb929f3c737a43
u-Alice <umugwanezaalice22@gmail.com>
2022-06-12 20:05:45 +0200

    completed fetch books

--- frontend/src/components/home/displayBooks.jsx
+++ frontend/src/components/home/displayBooks.jsx
@@ -1,86 +1,41 @@
-// import { GetBooks } from "./booksContext.js";
-function DisplayBooks(){
-const GetBooks = async ()=>{
-  await fetch("http://localhost:4001/getBooks", {
-      method:"GET"
-  }).then((res)=>{
-      return res.json()
-  }).then(data=>{
-      console.log(data)
-  })
-}
-GetBooks()
+import { useState } from "react";
+function DisplayBooks() {
+  const [books, setBooks] = useState([]);
+  const GetBooks = async () => {
+    await fetch("http://localhost:4001/getBooks", {
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBooks(data.books);
+      });
+  };
+  GetBooks();
   return (
-    
     <div>
       <div className="heading">
         <h1>Trending Now</h1>
       </div>
-    <div className="list">
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
+      <div className="list">
+        {books.map((item) => {
+          return  (
+            <div className="listItem">
+            <div className="image">
+              <img src={item.imgUrl} alt="preview image" />
+            </div>
+            <div>
+              <p>{item.title}</p>
+              <button>View Summary</button>
+            </div>
+          </div>
+          )
+        })}
+
+       
       </div>
-      <div>
-        <p>Nadfasdfdsfajlkhljkkljhlsdfasdfame</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-      <div className="listItem">
-      <div className="image">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <p>Name</p>
-        <button>View Summary</button>
-      </div>
-      </div>
-    </div>
     </div>
   );
 }
