29a2da39970d8fa2863b22f0ec59b6fe2eb7647d
u-Alice <umugwanezaalice22@gmail.com>
2022-06-19 20:24:54 +0200

    completed upload image

--- frontend/src/components/home/displayBooks.jsx
+++ frontend/src/components/home/displayBooks.jsx
@@ -6,6 +6,7 @@ function DisplayBooks() {
   const [horror, setHorror] = useState([]);
 
   const token = localStorage.getItem("token");
+  console.log(token)
   const GetBooks = async () => {
     await fetch("http://localhost:4001/getBooks", {
       method: "GET",
--- frontend/src/components/signup/uploadImage.jsx
+++ frontend/src/components/signup/uploadImage.jsx
@@ -1,29 +1,54 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function UploadImage(){
-    const [file, setFile] = useState("")
-    const token = localStorage.getItem("token")
-    const handleFileChange = (e) => {
-      setFile(e.target.files[0])
+function UploadImage() {
+  const [file, setFile] = useState();
+  const [imageUrl, setImageUrl] = useState("");
+  const token = localStorage.getItem("token");
+  const handleFileChange = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    previewFile(image);
+  };
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      const images = reader.result;
+      setImageUrl(reader.result);
+    };
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(imageUrl);
+    const formData = new FormData();
+    formData.append("files", file);
+    // formData.append("upload_preset", "Novelia");
+    try {
+      fetch("http://localhost:4001/addProfile", {
+        method: "POST",
+        body: JSON.stringify({data: imageUrl}),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.NjJhNWYwNzM0MWQyMjc4NGEwZDI3YTc1.gYlmT0YKnUivDWeaoE1VgKtbSMLZEyBEUNbn8MUyWsQ`,
+        },
+      });
+    } catch (err) {
+      console.log(err);
     }
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        const formData = new FormData()
-        fetch("http://localhost:4001/addProfile",{
-            method: "POST",
-            body:formData,
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        })
-    }
-    return(
-        <div>
-            <form action="" onSubmit={handleSubmit}>
-                <input type="file" name="image" onChange={handleFileChange}/>
-                <button type="submit">UPLOAD</button>
-            </form>
-        </div>
-    )
+  };
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="file"
+          name="image"
+          onChange={handleFileChange}
+          multiple
+          accept="images/*"
+        />
+        <button type="submit">UPLOAD</button>
+      </form>
+    </div>
+  );
 }
 export default UploadImage;
