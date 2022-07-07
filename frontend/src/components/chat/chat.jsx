0c89ca2b61f7590705bc923f6047688d88d9295e
u-Alice <umugwanezaalice22@gmail.com>
2022-07-07 11:53:48 +0200

    initialized frontend socket

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import io from "socket.io-client";
-function Chat(){
-    const socket = io.connect("http://localhost:4002");
-    return (
-        <div>
-            <h1>Join A chat</h1>
-        Name <input type="text" /> 
-        </div>
-    )
+function Chat() {
+  const socket = io.connect("http://localhost:4002");
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
+  const joinRoom = () => {
+    if(username !== "" && room !== ""){
+
+    }
+  };
+  return (
+    <div>
+      <h1>Join A chat</h1>
+      Name{" "}
+      <input
+        type="text"
+        placeholder="john.."
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+      />
+      Name{" "}
+      <input
+        type="text"
+        placeholder="Room Id.."
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join A Room</button>
+    </div>
+  );
 }
 export default Chat;
