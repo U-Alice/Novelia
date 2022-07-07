815cfe77fa695fa148979e5f3bbeec298e300c5b
u-Alice <umugwanezaalice22@gmail.com>
2022-07-07 13:50:30 +0200

    send Message

--- frontend/src/components/chat/chatItem.jsx
+++ frontend/src/components/chat/chatItem.jsx
@@ -1,15 +1,34 @@
-function ChatItem ({socket, room , username}){
-    return (
-        <div>   
-        <div className="chat-header">
-            <p>live Chat</p>
-        </div>
-        <div className="chat-body"></div>
-        <div className="chat-footer">
-            <input type="text" placeholder="Hey.." />
-            <button>&#9658;</button>
-        </div>
-        </div>
-    )
+import { useState } from "react";
+
+function ChatItem({ socket, room, username }) {
+  const [currentMessage, setCurrentMessage] = useState();
+  const sendMessage = () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author : username, 
+        message : currentMessage, 
+        time : new Date(Date.now()).getHours() + ":" + new Date(Date.now())
+      }
+    }
+  };
+  return (
+    <div>
+      <div className="chat-header">
+        <p>live Chat</p>
+      </div>
+      <div className="chat-body"></div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Hey.."
+          onChange={(e) => {
+            setCurrentMesaage(e.target.value);
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
 }
 export default ChatItem;
